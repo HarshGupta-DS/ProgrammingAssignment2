bbf9ede1699da62f02056d3026fa5b2da1b89e78
Harsh Gupta <36688558+HarshGupta-DS@users.noreply.github.com>
2021-05-29 12:40:20 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        B <- matrix(c(1,2,3,4),2,2)
+        
 }
+> B1<- makeCacheMatrix(B)
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+        cacheSolve(B1) 
+        #inverse returned after computation
         ## Return a matrix that is the inverse of 'x'
 }
+>   cacheSolve(B1)
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> cacheSolve(B1)
+getting cached data.
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
